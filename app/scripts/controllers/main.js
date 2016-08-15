1de2fae7c176e8958b94b5c79cf40bbe6a69acc3
Joel Rahman <joel@flowmatters.com.au>
2016-08-15 16:38:41 +1000

    Scaffolded with yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flammabilityApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the flammabilityApp
+ */
+angular.module('flammabilityApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
