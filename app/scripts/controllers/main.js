2263a9623323a2624f7b1271854ae4eac5f8688a
Joel Rahman <joel@flowmatters.com.au>
2016-10-31 14:20:15 +1100

    Updated to use new, WGS84 tiles

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,39 @@ angular.module('flammabilityApp')
   .controller('MainCtrl', function ($scope) {
     var WMS_SERVER = 'http://hydrograph.flowmatters.com.au';
 
+
+    $scope.bounds = {
+      northEast: {
+          lat: -0,
+          lng: 175
+      },
+      southWest: {
+        lat: -55,
+        lng: 90
+      }
+    };
+
+    $scope.year = 2015;
+    $scope.tiles = [
+      'h27v11',
+      'h27v12',
+      'h28v11',
+      'h28v12',
+      'h28v13',
+      'h29v10',
+      'h29v11',
+      'h29v12',
+      'h29v13',
+      'h30v10',
+      'h30v11',
+      'h30v12',
+      'h31v10',
+      'h31v11',
+      'h31v12',
+      'h32v10',
+      'h32v11',
+    ];
+
     $scope.layers={
       overlays:{
         mask: {
@@ -27,23 +60,24 @@ angular.module('flammabilityApp')
             zIndex:100,
             tiled:true
           }
-        },  //overlays.mask
-        fmc: {
-          name: 'Fuel Moisture Conent',
-//          url: 'http://dapds00.nci.org.au/thredds/wms/ub8/au/FMC/MCD43A4.A2014329.h29v13.005.2014347051300_LFMC.nc?',
-//          url: 'http://localhost:8080/thredds/wms/wald/ROI_Reprojected_Mosaic_LFMC_2015321.nc?',
-          url: 'http://dapds00.nci.org.au/thredds/wms/ub8/au/FMC-testing/ROI_Reprojected_Mosaic_LFMC_2015321.nc?',
-          // service=WMS&version=1.3.0&request=GetCapabilities
-          type: 'wms',
-          visible: true,
-          layerParams:{
-            version: '1.3.0',
-            format: 'image/png',
-            layers:'Band1',
-            colorscalerange:'0,350',
-            transparent:true
-          }
         }
       }
-    }
+    };
+
+    $scope.tiles.forEach(function(tile){
+      $scope.layers.overlays['fmc_'+tile] = {
+        name: 'Fuel Moisture Conent',
+        url: 'http://dapds00.nci.org.au/thredds/wms/ub8/au/FMC/wgs84/WGS84.MCD43A4.'+$scope.year+'.'+tile+'.005.LFMC.nc?',
+        type: 'wms',
+        visible: true,
+        layerParams:{
+          version: '1.3.0',
+          format: 'image/png',
+          layers:'lfmc_mean',
+          colorscalerange:'0,250',
+          abovemaxcolor:'extend',
+          transparent:true
+        }
+      };
+    });
   });
